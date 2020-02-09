60266d92df901c1e4f6f13e09245fd3b77c84413
Suzuki Takeo <takeo@stko.info>
2020-02-09 13:58:06 +0900

    build package

--- build.sbt
+++ build.sbt
@@ -2,11 +2,12 @@ import Settings._
 
 lazy val boot = (project in file("boot"))
   .settings(commonSettings)
+  .settings(sbtSettings)
   .settings(
     libraryDependencies ++= Seq()
   )
 
-
 lazy val root =
   (project in file("."))
     .aggregate(boot)
+    .settings(sbtSettings)
--- project/Settings.scala
+++ project/Settings.scala
@@ -8,10 +8,6 @@ object Settings {
   val sdk11 = "adoptopenjdk/openjdk11:x86_64-alpine-jdk-11.0.4_11-slim"
 
   lazy val commonSettings = Seq(
-    organization := "com.github.BambooTuna",
-    scalaVersion := "2.12.8",
-    version := "1.0.0-SNAPSHOT",
-    publishTo := Some(Resolver.file("CryptoExchangeAPI",file("."))(Patterns(true, Resolver.mavenStyleBasePattern))),
     libraryDependencies ++= Seq(
       Circe.core,
       Circe.generic,
@@ -29,6 +25,14 @@ object Settings {
     ),
     scalafmtOnCompile in Compile := true,
     scalafmtTestOnCompile in Compile := true
+  )
+
+  lazy val sbtSettings = Seq(
+    organization := "com.github.BambooTuna",
+    scalaVersion := "2.12.8",
+    version := "1.0.0-SNAPSHOT",
+    name := "CryptoExchangeAPI",
+    publishTo := Some(Resolver.file("CryptoExchangeAPI",file("."))(Patterns(true, Resolver.mavenStyleBasePattern)))
   )
 
 }
